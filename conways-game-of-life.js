ce549ba6697ab95fa4acad8b53781a9e56986733
MooMooCow1562 <106333842+MooMooCow1562@users.noreply.github.com>
2025-05-04 14:33:10 -0500

    implemented randomizing the board
    have not implemented game logic yet.
    Also I entirely misunderstood how => functions worked and switched back to the old style of functions.
    Changed the create 2d array function to allow for 2 parameters, allowing for oddly sized arrays.
    Added a draw cell function, which draws a cell based on a value referenced from the currentGrid.
    Added "flattenCoordinate", to round input coordinates to the cell cize in anticipation of adding click to change cell value. Also useful for the draw cell function, allowing us to start at a correct coordinate.
    Implemented randomize board, allowing one to click and randomize the current board (and subsequently draw it to the screen.)
    Added a drawBoard function, which simply draws the board using the drawCell function.

--- conways-game-of-life.js
+++ conways-game-of-life.js
@@ -1,3 +1,9 @@
+//Will replace the commenting with more robust descriptions later, like, block comments.
+
+//hardcoding cell values
+const alive = 2;
+const ghost = 1;
+const dead = 0;
 
 //hardcoding the color values for now.
 var livingColor = "#ffffff"; // Color of living cells
@@ -16,17 +22,58 @@ var context = canvas.getContext("2d");
 var cellSize = 10;
 
 //fill the canvas with black, to represent the initial dead state of the game
-context.fillStyle = "#000000"; // Background color
+context.fillStyle = deadColor; // Background color
 context.fillRect(0, 0, canvas.width, canvas.height); // Fill the canvas with the background color
+
 //our grids, Conway's game of life needs at least 2 grids and I beleive the names explan them.
-var currentGrid = create2dArray(canvas.width / cellSize);
-var nextGrid = create2dArray(canvas.width / cellSize);
+var currentGrid = create2dArray(canvas.width / cellSize, canvas.height / cellSize);
+var nextGrid = create2dArray(canvas.width / cellSize, canvas.height / cellSize);
 
 //Need a 2D array, Javascript unfortunately does not have an easy way to make these so I'll just make a function to do it :D
-create2dArray = (length) => {
-    let arr = new Array(length);
-    for (let i = 0; i < length; i++) {
+function create2dArray(length, height) {
+    let arr = new Array(height);
+    for (let i = 0; i < height; i++) {
         arr[i] = new Array(length).fill(0);
     }
     return arr;
+}
+
+//draws a cell, given two coordinates, starting from the flattened coordinates and ending at the flattened coordinate + cell size.
+//written to expect x and y values of a mouse clicking on the canvas, in other contexts like loops, I will need to multiply the coords by 10.
+function drawCell(x, y) {
+    normX = flattenCoordinate(x);
+    normY = flattenCoordinate(y);
+    if (currentGrid[normX / 10][normY / 10] == alive) {
+        context.fillStyle = livingColor;
+    } else if (currentGrid[normX / 10][normY / 10] == ghost) {
+        context.fillStyle = ghostColor;
+    } else {
+        context.fillStyle = deadColor;
+    }
+    context.fillRect(normY, normX, normY + cellSize, normX + cellSize);
+}
+
+//Takes a coordinate and rounds it to the 10ths place.
+function flattenCoordinate(v) {
+    return (Math.floor(v / cellSize)) * cellSize;
+}
+
+function randomizeBoard() {
+    for (let i = 0; i < (canvas.height / cellSize); i++) {
+        for (let j = 0; j < (canvas.width / cellSize); j++) {
+            var newVal = Math.floor(Math.random() * 3);//a random value between 0 and 2, hopefully.
+            currentGrid[i][j] = newVal;
+        }
+    }
+    //draw the board after randomizing the boards.
+    drawBoard();
+}
+
+//draws board separately from the computations for the board.
+function drawBoard(){
+    for (let i = 0; i < (canvas.height / cellSize); i++) {
+        for (let j = 0; j < (canvas.width / cellSize); j++) {
+            drawCell(i * 10, j * 10);
+        }
+    }
 }
